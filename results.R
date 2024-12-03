2cc9dc3ee6f6269c84625f2412724618643c074c
Jeroen Ooms <jeroenooms@gmail.com>
2024-12-03 15:45:15 +0100

    Factor check results into script

---
+++ results.R
@@ -0,0 +1,8 @@
+first <- tools:::check_packages_in_dir_results('.')[1]
+package <- names(first)
+results <- first[[1]]
+writeLines(paste0('CHECKSTATUS=',results$status),Sys.getenv("GITHUB_OUTPUT"))
+writeLines(results$lines, 'checkresults.txt')
+out <- lapply(grep("ERROR", results$lines, value=TRUE), function(errmsg){
+  cat(sprintf("::error file=%s::%s\n", package, gsub("\\s+", " ", errmsg)))
+})
