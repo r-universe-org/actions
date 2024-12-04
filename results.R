8799312aec21eb53b4b6cbcebe6deb505016e09b
Jeroen Ooms <jeroenooms@gmail.com>
2024-12-04 12:28:36 +0100

    Use multi-line annotations with full errors

--- results.R
+++ results.R
@@ -3,6 +3,12 @@ package <- names(first)
 results <- first[[1]]
 writeLines(paste0('CHECKSTATUS=',results$status),Sys.getenv("GITHUB_OUTPUT"))
 writeLines(results$lines, 'checkresults.txt')
-out <- lapply(grep("ERROR", results$lines, value=TRUE), function(errmsg){
-  cat(sprintf("::error file=%s::R CMD check reported problem: %s\n", package, gsub("\\s+", " ", errmsg)))
-})
+if(results$status == 'ERROR'){
+  details <- tools:::check_packages_in_dir_details('.')
+  writeLines(paste(format(details), collapse = "\n\n"), "checkdetails.txt")
+  errors <- details[details$Status=='ERROR',]
+  errors$Flags <- NULL
+  errors$Package <- NULL
+  errmsg <- paste(format(errors), collapse = "\n\n")
+  cat(sprintf("::error file=%s::%s\n", package, gsub("\n", "%0A", errmsg, fixed = TRUE)))
+}
