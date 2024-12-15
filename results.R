87c8a2f7de8869a71c29366255084e0e33b1cdd8
Jeroen Ooms <jeroenooms@gmail.com>
2024-12-15 22:32:22 +0000

    Show warnings if no errors only

--- results.R
+++ results.R
@@ -3,6 +3,7 @@ package <- names(first)
 results <- first[[1]]
 writeLines(paste0('CHECKSTATUS=',results$status),Sys.getenv("GITHUB_OUTPUT"))
 writeLines(results$lines, 'checkresults.txt')
+
 if(results$status == 'ERROR'){
   details <- tools:::check_packages_in_dir_details('.')
   writeLines(paste(format(details), collapse = "\n\n"), "checkdetails.txt")
@@ -11,4 +12,12 @@ if(results$status == 'ERROR'){
   errors$Package <- NULL
   errmsg <- paste(format(errors), collapse = "\n\n")
   cat(sprintf("::error file=%s::%s\n", package, gsub("\n", "%0A", errmsg, fixed = TRUE)))
+} else if(results$status == 'WARNING'){
+  details <- tools:::check_packages_in_dir_details('.')
+  writeLines(paste(format(details), collapse = "\n\n"), "checkdetails.txt")
+  errors <- details[details$Status=='WARNING',]
+  errors$Flags <- NULL
+  errors$Package <- NULL
+  errmsg <- paste(format(errors), collapse = "\n\n")
+  cat(sprintf("::warning file=%s::%s\n", package, gsub("\n", "%0A", errmsg, fixed = TRUE)))
 }
