a9c95facc8b3dab0fcfdc307cf9f41be431c95de
Jeroen Ooms <jeroenooms@gmail.com>
2025-04-21 11:43:09 +0200

    Tweak name of conclude step

--- deploy-packages/jobsdata.sh
+++ deploy-packages/jobsdata.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 
 echo "::group::getting jobs info"
 curl -s -D /dev/stderr -H "Authorization: token ${GITHUB_TOKEN}" --fail "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/jobs" | \
-  jq -r '[.jobs[] | {job: .id, result: .steps[] | select(.name | startswith("Set:")).name | split(": ") } | {job, config: .result[1], r: .result[2], check: .result[3]}]' |
+  jq -r '[.jobs[] | {job: .id, result: .steps[] | select(.name | startswith("Conclude:")).name | split(": ") } | {job, config: .result[1], r: .result[2], check: .result[3]}]' |
   gzip | openssl base64 -A -out jobsdata.txt
 echo "::endgroup::"
