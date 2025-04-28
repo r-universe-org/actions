de03336e9b6d7d5353d9d48c25eda4d07c505cd9
Jeroen Ooms <jeroenooms@gmail.com>
2025-04-28 11:58:12 +0200

    Drop status variables in favor of new _jobs (#2)
    * Drop status variables in favor of new _jobs
    
    * Also remove separate pkgdocs status

--- deploy-packages/deploy.sh
+++ deploy-packages/deploy.sh
@@ -72,50 +72,7 @@ fi
 
 # Add status for binaries to source deploy
 if [ "$PKGTYPE" == "src" ]; then
-
-# TODO: this should replace all the STATUS variables below
 JOBSDATA=$(cat ../jobsdata.txt)
-
-if [ -d "../package-macos-release" ]; then
-MACOS_BINARY_STATUS=$(cd ../package-macos-release; (source pkgdata.txt; echo "$JOB_STATUS")) # no longer used
-elif [ -d "../package-macos-release-arm64" ]; then
-MACOS_BINARY_STATUS=$(cd ../package-macos-release-arm64; (source pkgdata.txt; echo "$JOB_STATUS"))
-elif [ -d "../package-macos-release-x86_64" ]; then
-MACOS_BINARY_STATUS="arm64-failure"
-elif [ "$SKIP_BINARIES" ]; then
-MACOS_BINARY_STATUS="skipped"
-else
-MACOS_BINARY_STATUS="none"
-fi
-
-if [ -d "../package-windows-devel" ]; then
-WINDOWS_DEVEL_STATUS=$(cd ../package-windows-devel; (source pkgdata.txt; echo "$JOB_STATUS"))
-elif [ "$SKIP_BINARIES" ]; then
-WINDOWS_DEVEL_STATUS="skipped"
-else
-WINDOWS_DEVEL_STATUS="none"
-fi
-
-if [ -d "../package-windows-release" ]; then
-WINDOWS_BINARY_STATUS=$(cd ../package-windows-release; (source pkgdata.txt; echo "$JOB_STATUS"))
-elif [ "$SKIP_BINARIES" ]; then
-WINDOWS_BINARY_STATUS="skipped"
-else
-WINDOWS_BINARY_STATUS="none"
-fi
-
-if [ -d "../package-linux-devel" ]; then
-LINUX_DEVEL_STATUS=$(cd ../package-linux-devel; (source pkgdata.txt; echo "$JOB_STATUS"))
-else
-LINUX_DEVEL_STATUS="none"
-fi
-
-if [ -d "../package-wasm-release" ]; then
-WASM_BINARY_STATUS=$(cd ../package-wasm-release; (source pkgdata.txt; echo "$JOB_STATUS"))
-else
-WASM_BINARY_STATUS="none"
-fi
-
 fi
 
 upload_package_file(){
@@ -130,13 +87,7 @@ upload_package_file(){
 		-H "Builder-Host: GitHub-Actions" \
 		-H "Builder-Status: ${JOB_STATUS}" \
 		-H "Builder-Check: ${CHECKSTATUS}" \
-		-H "Builder-Pkgdocs: ${PKGDOCS}" \
 		-H "Builder-Srconly: ${SKIP_BINARIES}" \
-		-H "Builder-Winbinary: ${WINDOWS_BINARY_STATUS}" \
-		-H "Builder-Macbinary: ${MACOS_BINARY_STATUS}" \
-		-H "Builder-Wasmbinary: ${WASM_BINARY_STATUS}" \
-		-H "Builder-Linuxdevel: ${LINUX_DEVEL_STATUS}" \
-		-H "Builder-Windevel: ${WINDOWS_DEVEL_STATUS}" \
 		-H "Builder-Buildurl: https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}" \
 		-H 'Expect:' \
 		"${SERVERURL}/${SHASUM}" &&\
