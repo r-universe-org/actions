b150c6fb63a2bafa54a09f86cd7d63b0455ab5fb
Jeroen Ooms <jeroenooms@gmail.com>
2025-06-29 21:47:41 +0200

    Check for version bumps during deploy

---
+++ deploy-packages/version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$UNIVERSE_NAME" == "cran" ] || [ "$UNIVERSE_NAME" == "bioc" ]; then
+echo "Skipping version bump check for cran/bioc repos."
+exit 0
+fi
+
+OLDVERSION=$(curl -sSf "https://${UNIVERSE_NAME}.r-universe.dev/api/packages/${PACKAGE}" | jq -r '.Version')
+if [ "$OLDVERSION" ] && [ "$OLDVERSION" != "$VERSION" ]; then
+  echo "Version change from $OLDVERSION to $VERSION"
+  echo "version_change=true" >> $GITHUB_OUTPUT
+else
+  echo "Version $VERSION has not changed."
+fi
+
+exit 0
