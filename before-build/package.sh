c849f0f00d590519582ac45495ab08d82c42b6bf
Jeroen Ooms <jeroenooms@gmail.com>
2026-01-07 14:20:30 +0100

    Change default for binaries because "false" is not false

--- before-build/package.sh
+++ before-build/package.sh
@@ -23,9 +23,8 @@ UNIVERSE="$(basename $GITHUB_REPOSITORY)"
 REPOSITORY=$(git config --list | grep "submodule.${PACKAGE}.url=" | cut -d'=' -f2)
 REF=$(git submodule status $PACKAGE | awk '{print $1}' | sed 's/^[^0-9a-z]*//')
 SUBDIR=$(git config -f .gitmodules --get "submodule.${PACKAGE}.subdir" || true)
-BINARIES="true"
 if [ "$GITHUB_REPOSITORY" = "r-universe/cran" ]; then
-  BINARIES="false"
+NOBINARIES="true"
 fi
 
 # Check for remotes
@@ -60,7 +59,7 @@ echo "universe=$UNIVERSE" | tee -a $GITHUB_OUTPUT
 echo "repository=$REPOSITORY" | tee -a $GITHUB_OUTPUT
 echo "subdir=$SUBDIR" | tee -a $GITHUB_OUTPUT
 echo "ref=$REF" | tee -a $GITHUB_OUTPUT
-echo "binaries=$BINARIES" | tee -a $GITHUB_OUTPUT
+echo "nobinaries=$NOBINARIES" | tee -a $GITHUB_OUTPUT
 echo "organization=$ORGANIZATION" | tee -a $GITHUB_OUTPUT
 echo "registered=$REGISTERED" | tee -a $GITHUB_OUTPUT
 echo "hasapp=$HASAPP" | tee -a $GITHUB_OUTPUT
