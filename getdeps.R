872fae8dc2acc7be9b160dfcca721bbbeb146da3
Jeroen Ooms <jeroenooms@gmail.com>
2026-02-27 13:51:05 +0100

    Installing R and Rtools on arm64 Windows

--- getdeps.R
+++ getdeps.R
@@ -66,7 +66,7 @@ options("install.packages.compile.from.source"="never")
 
 # Somehow 'install.packages.check.source=no' still installs packages w/o compiled code from src
 # But we also need transitive deps that are not available as binary such as bioconductor data packages
-if(.Platform$OS.type == 'windows'){
+if(.Platform$OS.type == 'windows' && identical(.Platform$r_arch, 'x64')){
   install.packages(pkg_deps, type = 'win.binary')
   alldeps <- unique(unname(c(pkg_deps, unlist(tools::package_dependencies(pkg_deps, recursive = TRUE)))))
   missingdeps <- setdiff(alldeps, c(skiplist, row.names(installed.packages())))
