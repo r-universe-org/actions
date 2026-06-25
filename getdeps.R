1f3e7cabdc1fa4e099434dcc47dd4ac5e527bbc1
Jeroen Ooms <jeroenooms@gmail.com>
2026-06-25 14:18:13 +0200

    Disable additional repositories for cran/bioc

--- getdeps.R
+++ getdeps.R
@@ -36,8 +36,12 @@ if(length(desc$Additional_repositories)){
   addrepos <- trimws(strsplit(desc$Additional_repositories, ",", fixed=TRUE)[[1]])
   addrepos <- grep('^https?://\\S+$', addrepos, value = TRUE)
   addrepos <- grep(Sys.getenv("MY_UNIVERSE"), fixed = TRUE, addrepos, value = TRUE, invert = TRUE)
-  message("Additional_repositories: ", paste(addrepos, collapse = ', '))
-  options(repos = c(getOption('repos'), addrepos))
+  if(nchar(Sys.getenv('NO_ADDITIONAL_REPOSITORIES'))){
+    message("Additional_repositories are ignored for this organization!")
+  } else {
+    message("Additional_repositories: ", paste(addrepos, collapse = ', '))
+    options(repos = c(getOption('repos'), addrepos))
+  }
 }
 
 # Install sysdeps (Linux only)
